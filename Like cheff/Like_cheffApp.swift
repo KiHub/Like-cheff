1d496104561758063f52d931cb2b035c40c03715
Kirill Ki <kirillkalapov@gmail.com>
2022-07-15 13:19:07 +0200

    Initial Commit

---
+++ Like cheff/Like_cheffApp.swift
@@ -0,0 +1,17 @@
+//
+//  Like_cheffApp.swift
+//  Like cheff
+//
+//  Created by  Mr.Ki on 15.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct Like_cheffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
